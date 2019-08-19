ffe413ea3f758ef1f15e0ff44c60c2486e3e56a6
EC2 Default User <ec2-user@ip-172-31-19-72.us-east-2.compute.internal>
2019-08-19 19:25:45 +0000

    Completed spotlight function

---
+++ oo_data_structures/week_4/project/ImageTransform.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cmath>
+
+#include "uiuc/PNG.h"
+using namespace uiuc;
+
+PNG grayscale(PNG image);  
+PNG createSpotlight(PNG image, int centerX, int centerY);
+PNG illinify(PNG image);
+PNG watermark(PNG firstImage, PNG secondImage);
