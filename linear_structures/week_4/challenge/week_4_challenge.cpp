f11db2aa3f453fbfe897088be13abbc685fa981d
EC2 Default User <ec2-user@ip-172-31-19-72.us-east-2.compute.internal>
2019-09-17 15:42:10 +0000

    Week 4 challenge

---
+++ linear_structures/week_4/challenge/week_4_challenge.cpp
@@ -0,0 +1,13 @@
+/*
+
+Implement below the downHeap procedure for a min heap.
+
+Unlike the video lessons which demonstrated downHeap
+implemented with an array implementation, this assignment
+uses an ordinary binary tree with left and right child
+pointers. This ordinary binary tree might not be complete
+or balanced: any node might have only one child or the other,
+or both, or neither.
+
+Suppose you are given a tree where the left and right
+subtrees are min heaps, but the root node does not
